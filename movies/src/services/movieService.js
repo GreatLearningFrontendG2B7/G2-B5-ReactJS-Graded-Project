2d9497dc90ee94df190a48184926a770276e92c3
iamsubendu <66836529+iamsubendu@users.noreply.github.com>
2024-08-26 22:37:11 +0530

    getting data now

--- movies/src/services/movieService.js
+++ movies/src/services/movieService.js
@@ -1,34 +1,23 @@
-import axios from 'axios';
-import data from '../data/data.js';
-
-// Replace with your actual API URL
-// const API_URL = 'https://api.example.com';
-
-// Alternatively, use a local JSON file for development
-//const API_URL = '../data/data.json';
-const API_URL = "http://localhost:3002/";
+import movieData from '../data/data';
 
 const getAllMovies = async () => {
-  const my_movies = `${API_URL}movies-coming`;
-  //const response = await axios.get(`${API_URL}/movies`);
-  const response = await axios.get(my_movies);
-  return response.data;
+  const my_movies = movieData['movies-coming'];
+  return my_movies;
 };
 
 const getMovieById = async (id) => {
-  const my_movie_by_id = `${API_URL}movies-coming${[id]}`;
-  const response = await axios.get(my_movie_by_id);
-  return response.data;
+  const my_movie_by_id = movieData['movies-coming'][id];
+  return my_movie_by_id;
 };
 
 const searchMovies = async (searchTerm) => {
-  const response = await axios.get(`${API_URL}/movies/search?q=${searchTerm}`);
-  return response.data;
+  // const response = await axios.get(`${API_URL}/movies/search?q=${searchTerm}`);
+  // return response.data;
 };
 
 const getFavorites = async () => {
   // Implement logic to fetch favorites from local storage or database
-  return [];
+  // return [];
 };
 
 // Export the functions directly for easier use
